46846220736e0f43838bdcd63bc86457f31b5c14
anurento <anurag.kumar@rentomojo.com>
2019-06-24 12:12:41 +0530

    new

--- src/components/Browser/Browser.vue
+++ src/components/Browser/Browser.vue
@@ -53,7 +53,7 @@
           </span>
         </div>
       </menu>
-      <div class="progress">
+      <div class="progress" v-if="loading">
         <div class="indeterminate"></div>
       </div>
       <media-main-content></media-main-content>

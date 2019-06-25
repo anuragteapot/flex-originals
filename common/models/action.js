34d77a004b65a40cb2a75db34972a6f5966f1132
anurento <anurag.kumar@rentomojo.com>
2019-06-25 14:00:07 +0530

    new

--- common/models/action.js
+++ common/models/action.js
@@ -1,49 +1,49 @@
 "use strict";
 
-var multer = require("multer");
-var fs = require("fs-extra");
+// var multer = require("multer");
+// var fs = require("fs-extra");
 
 module.exports = function(Action) {
-  const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      var dirPath = "uploads/";
-      if (!fs.existsSync(dirPath)) {
-        fs.mkdirSync(dirPath);
-      }
-      cb(null, dirPath + "/");
-    },
+  // const storage = multer.diskStorage({
+  //   destination: (req, file, cb) => {
+  //     var dirPath = "uploads/";
+  //     if (!fs.existsSync(dirPath)) {
+  //       fs.mkdirSync(dirPath);
+  //     }
+  //     cb(null, dirPath + "/");
+  //   },
 
-    filename: (req, file, cb) => {
-      cb(null, file.originalname);
-    }
-  });
+  //   filename: (req, file, cb) => {
+  //     cb(null, file.originalname);
+  //   }
+  // });
 
-  Action.upload = (req, res) => {
-    var upload = multer({
-      storage: storage
-    }).single("file");
+  // Action.upload = (req, res) => {
+  //   var upload = multer({
+  //     storage: storage
+  //   }).single("file");
 
-    upload(req, res, function(err) {
-      if (err) {
-        return res.json(err);
-      } else {
-        return res.json({
-          res: "done"
-        });
-      }
-    });
-  };
+  //   upload(req, res, function(err) {
+  //     if (err) {
+  //       return res.json(err);
+  //     } else {
+  //       return res.json({
+  //         res: "done"
+  //       });
+  //     }
+  //   });
+  // };
 
-  Action.remoteMethod("upload", {
-    description: "Upload a file",
-    accepts: [
-      { arg: "req", type: "object", http: { source: "req" } },
-      { arg: "res", type: "object", http: { source: "res" } }
-    ],
-    returns: {
-      arg: "result",
-      type: "string"
-    },
-    http: { verb: "post" }
-  });
+  // Action.remoteMethod("upload", {
+  //   description: "Upload a file",
+  //   accepts: [
+  //     { arg: "req", type: "object", http: { source: "req" } },
+  //     { arg: "res", type: "object", http: { source: "res" } }
+  //   ],
+  //   returns: {
+  //     arg: "result",
+  //     type: "string"
+  //   },
+  //   http: { verb: "post" }
+  // });
 };
--- common/models/user.js
+++
@@ -1,7 +0,0 @@
-/** @format */
-
-'use strict'
-
-module.exports = User => {
-
-}

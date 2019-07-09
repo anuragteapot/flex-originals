2cfe046e8933204b710b4aac1b8ad9361604f254
anu1601cs <anuragvns1111@gmail.com>
2019-07-09 22:14:51 +0530

    new

--- src/components/Browser/Upload/Upload.vue
+++ src/components/Browser/Upload/Upload.vue
@@ -7,159 +7,101 @@
             <div class="container">
               <div class="grid">
                 <h1>Upload Status</h1>
-              <i @click="$router.push('/app/@home')" class="far fa-times-circle close__settings"></i>
+                <i @click="$router.push('/app/@home')" class="far fa-times-circle close__settings"></i>
               </div>
             </div>
             <div class="container">
               <div class="grid grid--half">
                 <h3>Thumbnail</h3>
-                <div class="form-item">
-                  <div class="account__info">
-                    <div class="avatar">
-                      <img class="account__avatar" src="/public/atom.svg" />
-                      <!-- <span class="avatar__change">Change Avatar</span> -->
+                <div class="video__upload__progress">
+                  <div class="upload__progress">
+                    <p>10%</p>
+                  </div>
+                </div>
+                <div class="video__thumbnails">
+                  <img class="lazy__image" src="/public/qq.webp" alt="thumbnail" />
+                </div>
+                <div class="upload__video__settings">
+                  <div class="video__title">
+                    <label for="videoTitle">Video Title</label>
+                    <input name="videoTitle" type="text" placeholder="Video Title" />
+                  </div>
+                  <div class="video__description">
+                    <label for="videoDescription">Video Description</label>
+                    <textarea
+                      rows="10"
+                      name="videoDescription"
+                      type="text"
+                      placeholder="Video Description"
+                    />
+                  </div>
+                  <div class="video__tags">
+                    <label for="videoTags">Video Tags</label>
+                    <input name="videoTags" type="text" placeholder="Video Tags" />
+                  </div>
+                  <div class="form-item">
+                    <label class="form-item__label">Text information</label>
+                    <div class="form-item__control">
+                      <select class="control control--select">
+                        <option selected="selected">Public</option>
+                        <option>Private</option>
+                      </select>
                     </div>
-                    <div class="email__info">
-                      <p>Anurag Kumar</p>
-                      <p>anuragvns1111@gmail.com</p>
+                  </div>
+                  <div class="form-item">
+                    <label class="form-item__label">Premier</label>
+                    <div class="form-item__control toggle">
+                      <div class="toggle__handle"></div>
                     </div>
-                    <div class="username__info">
-                      <p>http://creatar.com/creator/@anu1601cs</p>
-                      <button class="info" @click="logout">Log Out</button>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-item">
-                  <label class="form-item__label">Enable Email Notifications</label>
-                  <div class="form-item__control toggle">
-                    <div class="toggle__handle"></div>
-                  </div>
-                </div>
-                <div class="form-item">
-                  <label class="form-item__label">Enable Desktop Notifications</label>
-                  <div class="form-item__control toggle">
-                    <div class="toggle__handle"></div>
-                  </div>
-                </div>
-                <div class="form-item">
-                  <label class="form-item__label">Theme ( Light/Dark )</label>
-                  <div class="form-item__control toggle">
-                    <div class="toggle__handle"></div>
-                  </div>
-                </div>
-                <div class="form-item">
-                  <label class="form-item__label">Show icon in dock</label>
-                  <div class="form-item__control toggle is-on">
-                    <div class="toggle__handle"></div>
-                  </div>
-                </div>
-                <div class="form-item">
-                  <label class="form-item__label">Hide &quot;Unlock Extreme&quot; button</label>
-                  <div class="form-item__control toggle">
-                    <div class="toggle__handle"></div>
-                  </div>
-                </div>
-                <h3>Desktop Status Menu</h3>
-                <div class="form-item">
-                  <label class="form-item__label">Use precentages</label>
-                  <div class="form-item__control toggle">
-                    <div class="toggle__handle"></div>
-                  </div>
-                </div>
-                <div class="form-item">
-                  <label class="form-item__label">Icon visible</label>
-                  <div class="form-item__control toggle">
-                    <div class="toggle__handle"></div>
-                  </div>
-                </div>
-                <div class="form-item">
-                  <label class="form-item__label">Text information</label>
-                  <div class="form-item__control">
-                    <select class="control control--select">
-                      <option selected="selected">Default</option>
-                      <option>Option 1</option>
-                      <option>Option 2</option>
-                      <option>Option 3</option>
-                    </select>
                   </div>
                 </div>
               </div>
               <div class="grid grid--half">
-                <h3>Performance</h3>
+                <h3>Advanced Settings</h3>
                 <div class="form-item">
-                  <label class="form-item__label">Threshold level</label>
-                  <div class="form-item__control">
-                    <small>
-                      <strong>
-                        <span class="slider__value">500</span>
-                        <span>mb</span>
-                      </strong>
-                    </small>
-                  </div>
-                  <div class="slider">
-                    <input class="slider__input" type="range" value="50" min="0" max="100" />
-                    <!-- <div ref="sliderPos" class="slider__positive" style="width: 50%;"></div>  -->
-                  </div>
-                </div>
-                <p>
-                  <small>If the available memory goes below this amount, the status bar text will turn red.</small>
-                </p>
-                <div class="form-item">
-                  <label class="form-item__label">Auto clean</label>
+                  <label class="form-item__label">Keep all my liked private</label>
                   <div class="form-item__control toggle">
                     <div class="toggle__handle"></div>
                   </div>
                 </div>
-                <p>
-                  <small>Automatically clean when available memory drops below the above threshold. Auto clean is limited to once every 3 minutes.</small>
-                </p>
-                <div class="form-item">
-                  <label class="form-item__label">Disable auto clean cooldown</label>
-                  <div class="form-item__control toggle">
-                    <div class="toggle__handle"></div>
-                  </div>
+                <div class="checkbox">
+                  <input id="one" type="checkbox" />
+                  <span class="check"></span>
+                  <label for="one">Allow Comments</label>
                 </div>
-                <p>
-                  <small>
-                    Auto clean cooldown:
-                    <strong>136 seconds</strong>
-                  </small>
-                </p>
+                <div class="checkbox">
+                  <input id="two" type="checkbox" />
+                  <span class="check"></span>
+                  <label for="two">Users can view ratings for this video</label>
+                </div>
+                <div class="checkbox">
+                  <input id="three" type="checkbox" />
+                  <span class="check"></span>
+                  <label for="three">Age restrictions</label>
+                </div>
                 <div class="form-item">
-                  <label class="form-item__label">Refresh interval</label>
+                  <label class="form-item__label">Category</label>
                   <div class="form-item__control">
-                    <small>
-                      <strong>
-                        <span class="slider__value">5</span>
-                        <span>&nbsp;seconds</span>
-                      </strong>
-                    </small>
+                    <select class="control control--select">
+                      <option selected="selected">People & Blog</option>
+                      <option>Gamming</option>
+                      <option>Education</option>
+                      <option>Comedy</option>
+                    </select>
                   </div>
                 </div>
-                <p>
-                  <small>If the available memory goes below this amount, the status bar text will turn red.</small>
-                </p>
-                <h3>Security</h3>
-                <p>Two-factor authentication</p>
-                <p>
-                  <small>This will sign you out of sessions in other browsers or on other computers.</small>
-                </p>
-                <button class="success">Enable Two-Factor Auth</button>
-
-                <p>Sign out of all other sessions</p>
-                <p>
-                  <small>This will sign you out of sessions in other browsers or on other computers.</small>
-                </p>
-                <button class="warning">Sign out other sessions</button>
-                <h3>Danger Zone</h3>
-                <p>
-                  <small>
-                    Delete this repository
-                    Once you delete a repository, there is no going back. Please be certain.
-                  </small>
-                </p>
-                <button class="danger">Deactivate Account</button>
-                <button class="danger">Delete Account</button>
+                <div class="form-item">
+                  <label class="form-item__label">Licence and rights ownership</label>
+                  <div class="form-item__control">
+                    <select class="control control--select">
+                      <option selected="selected">People & Blog</option>
+                      <option>Gamming</option>
+                      <option>Education</option>
+                      <option>Comedy</option>
+                    </select>
+                  </div>
+                </div>
+                <button class="success">Publish</button>
               </div>
             </div>
           </div>

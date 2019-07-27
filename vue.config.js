075d27771346722dcf7231a8202ebe3dd37ffd64
anu1601cs <anuragvns1111@gmail.com>
2019-07-27 12:08:48 +0530

    icons path update

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,22 @@
-const path = require('path')
+const path = require('path');
 module.exports = {
   outputDir: path.resolve(__dirname, './client/'),
   publicPath: '/',
+  pwa: {
+    name: 'Flex Originals',
+    themeColor: '#f72f2f',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'public/icons/favicon-32x32.png',
+      favicon16: 'public/icons/favicon-16x16.png',
+      appleTouchIcon: 'public/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'public/icons/safari-pinned-tab.svg',
+      msTileImage: 'public/icons/msapplication-icon-144x144.png'
+    }
+  },
   devServer: {
     proxy: 'http://localhost:3344'
   }
-}
+};

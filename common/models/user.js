7eaf91d943d57a06fe15af37f4f7d8964bd59c04
anu1601cs <anuragvns1111@gmail.com>
2019-07-27 15:35:05 +0530

    fix link

--- common/models/user.js
+++ common/models/user.js
@@ -1,28 +1,33 @@
-const config = require("../../server/config.json");
-const path = require("path");
-const app = require("../../server/server");
+const config = require('../../server/config.json');
+const path = require('path');
+const app = require('../../server/server');
 
 //Replace this address with your actual address
-const senderAddress = "noreply@flexoriginales.com";
+const senderAddress = 'noreply@flexoriginales.com';
 
 module.exports = function(User) {
-  User.afterRemote("create", function(context, user, next) {
+  User.afterRemote('create', function(context, user, next) {
     const Settings = app.models.Settings;
 
     var options = {
-      type: "email",
+      type: 'email',
       to: user.email,
       from: senderAddress,
-      subject: "Thanks for registering.",
-      template: path.resolve(__dirname, "../../server/views/mails/verify.ejs"),
-      host: "localhost",
-      port: process.env.NODE_ENV === "production" ? "80" : "8080",
-      redirect: "/",
-      restApiRoot: "/verify",
+      subject: 'Thanks for registering.',
+      template: path.resolve(__dirname, '../../server/views/mails/verify.ejs'),
+      host:
+        process.env.NODE_ENV === 'production'
+          ? 'http://ec2-13-127-196-184.ap-south-1.compute.amazonaws.com'
+          : 'localhost',
+      port: process.env.NODE_ENV === 'production' ? '80' : '8080',
+      redirect: '/',
+      restApiRoot: '/verify',
       user: user
     };
 
-    const profileAvatar = `/public/profile-icon/avatar${Math.floor(Math.random() * 28) + 1}.svg`;
+    const profileAvatar = `/public/profile-icon/avatar${Math.floor(
+      Math.random() * 28
+    ) + 1}.svg`;
 
     Settings.upsert({
       ownerId: user.id,
@@ -30,11 +35,11 @@ module.exports = function(User) {
       emailNotification: false,
       verifiedChannel: false,
       followers: 0,
-      facebook: "",
-      instagram: "",
-      twitter: "",
-      redit: "",
-      linkedin: "",
+      facebook: '',
+      instagram: '',
+      twitter: '',
+      redit: '',
+      linkedin: '',
       profileAvatar
     });
 
@@ -44,47 +49,47 @@ module.exports = function(User) {
         return next(err);
       }
       context.result = {
-        title: "Signed up successfully",
+        title: 'Signed up successfully',
         content:
-          "Please check your email and click on the verification link " +
-          "before logging in.",
-        redirectTo: "/"
+          'Please check your email and click on the verification link ' +
+          'before logging in.',
+        redirectTo: '/'
       };
       return next();
     });
   });
 
   // Method to render
-  User.afterRemote("prototype.verify", function(context, user, next) {
+  User.afterRemote('prototype.verify', function(context, user, next) {
     context.result = {
       title:
-        "A Link to reverify your identity has been sent " +
-        "to your email successfully",
+        'A Link to reverify your identity has been sent ' +
+        'to your email successfully',
       content:
-        "Please check your email and click on the verification link " +
-        "before logging in",
-      redirectTo: "/"
+        'Please check your email and click on the verification link ' +
+        'before logging in',
+      redirectTo: '/'
     };
     return next();
   });
 
-  User.afterRemote("prototype.confirm", function(ctx, next) {
-    console.log("Cannot speak! after", ctx);
+  User.afterRemote('prototype.confirm', function(ctx, next) {
+    console.log('Cannot speak! after', ctx);
     next();
   });
 
-  User.beforeRemote("prototype.confirm", function(ctx, next) {
-    console.log("Cannot speak before!", ctx);
+  User.beforeRemote('prototype.confirm', function(ctx, next) {
+    console.log('Cannot speak before!', ctx);
     next();
   });
 
   //send password reset link when requested
-  User.on("resetPasswordRequest", function(ctx) {
-    var url = "http://" + config.host + ":" + config.port + "/reset-password";
+  User.on('resetPasswordRequest', function(ctx) {
+    var url = 'http://' + config.host + ':' + config.port + '/reset-password';
     var html =
       'Click <a href="' +
       url +
-      "?access_token=" +
+      '?access_token=' +
       ctx.accessToken.id +
       '">here</a> to reset your password';
 
@@ -92,12 +97,12 @@ module.exports = function(User) {
       {
         to: ctx.email,
         from: senderAddress,
-        subject: "Password reset",
+        subject: 'Password reset',
         html: html
       },
       function(err) {
-        if (err) return console.log("> error sending password reset email");
-        console.log("> sending password reset email to:", ctx.email);
+        if (err) return console.log('> error sending password reset email');
+        console.log('> sending password reset email to:', ctx.email);
       }
     );
   });
@@ -105,34 +110,34 @@ module.exports = function(User) {
   User.findSetting = async id => {
     const Settings = app.models.Settings;
     if (!id) {
-      throw new Error("User Id is required.", {}, 500);
+      throw new Error('User Id is required.', {}, 500);
     }
     return await Settings.findOne({ where: { ownerId: id } });
   };
 
-  User.remoteMethod("findSetting", {
-    description: "Method to get the user settings",
+  User.remoteMethod('findSetting', {
+    description: 'Method to get the user settings',
     accepts: [
       {
-        arg: "id",
-        type: "string",
+        arg: 'id',
+        type: 'string',
         required: true
       }
     ],
     returns: {
-      type: "object",
+      type: 'object',
       root: true
     },
     http: {
-      path: "/findSetting/:id",
-      verb: "get"
+      path: '/findSetting/:id',
+      verb: 'get'
     }
   });
 
   User.updateSetting = async (id, newSettings) => {
     const Settings = app.models.Settings;
     if (!id) {
-      throw new Error("User Id is required.", {}, 500);
+      throw new Error('User Id is required.', {}, 500);
     }
     let userSettings = await Settings.findOne({ where: { ownerId: id } });
     userSettings.desktopNotification = newSettings.desktopNotification;
@@ -148,48 +153,48 @@ module.exports = function(User) {
     return userSettings.save();
   };
 
-  User.remoteMethod("updateSetting", {
-    description: "Method to get the user settings",
+  User.remoteMethod('updateSetting', {
+    description: 'Method to get the user settings',
     accepts: [
       {
-        arg: "id",
-        type: "string",
+        arg: 'id',
+        type: 'string',
         required: true
       },
       {
-        arg: "newSettings",
-        type: "object",
+        arg: 'newSettings',
+        type: 'object',
         required: true
       }
     ],
     returns: {
-      type: "object",
+      type: 'object',
       root: true
     },
     http: {
-      path: "/updateSettings",
-      verb: "post"
+      path: '/updateSettings',
+      verb: 'post'
     }
   });
 
   //render UI page after password change
-  User.afterRemote("changePassword", function(context, user, next) {
+  User.afterRemote('changePassword', function(context, user, next) {
     context.result = {
-      title: "Password changed successfully",
-      content: "Please login again withu new password",
-      redirectTo: "/",
-      redirectToLinkText: "Log in"
+      title: 'Password changed successfully',
+      content: 'Please login again withu new password',
+      redirectTo: '/',
+      redirectToLinkText: 'Log in'
     };
     return next();
   });
 
   //render UI page after password reset
-  User.afterRemote("setPassword", function(context, user, next) {
+  User.afterRemote('setPassword', function(context, user, next) {
     context.result = {
-      title: "Password reset success",
-      content: "Your password has been reset successfully",
-      redirectTo: "/",
-      redirectToLinkText: "Log in"
+      title: 'Password reset success',
+      content: 'Your password has been reset successfully',
+      redirectTo: '/',
+      redirectToLinkText: 'Log in'
     };
     return next();
   });

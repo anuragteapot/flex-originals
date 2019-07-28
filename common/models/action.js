508523b066a12ad3c4782353a09bac36b951f14f
anu1601cs <anuragvns1111@gmail.com>
2019-07-28 10:28:05 +0530

    new video

--- common/models/action.js
+++ common/models/action.js
@@ -1,49 +1,78 @@
-'use strict'
+'use strict';
 
-const multer = require('multer')
-const fs = require('fs-extra')
+const multer = require('multer');
+const fs = require('fs-extra');
+const helper = require('./../../server/helper-util');
+const path = require('path');
+const app = require('../../server/server');
 
 module.exports = function(Action) {
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      const dirPath = 'uploads/'
+      const userId = req.params.id;
+      const dirPath = `uploads/${userId}`;
       if (!fs.existsSync(dirPath)) {
-        fs.mkdirSync(dirPath)
+        fs.mkdirSync(dirPath);
       }
-      cb(null, dirPath + '/')
+      const sectionPath = path.join(dirPath, helper.randomId());
+      fs.mkdirSync(sectionPath);
+      const filePath = path.join(sectionPath, req.params.type);
+      fs.mkdirSync(filePath);
+      cb(null, filePath);
     },
 
     filename: (req, file, cb) => {
-      cb(null, file.originalname)
+      cb(null, file.originalname);
     }
-  })
+  });
 
   Action.upload = (req, res) => {
+    const Videos = app.models.Videos;
     const upload = multer({
       storage: storage
-    }).single('file')
+    }).single('file');
 
-    upload(req, res, function(err) {
+    upload(req, res, async err => {
       if (err) {
-        return res.json(err)
+        return res.json(err);
       } else {
-        return res.json({
-          res: 'done'
-        })
+        try {
+          const video = await Videos.create({
+            videoOwnerId: req.params.id,
+            videoFile: req.file.path,
+            name: req.file.originalname,
+            videoMeta: req.file
+          });
+          return res.json({
+            video
+          });
+        } catch (err) {
+          return res.json(err);
+        }
       }
-    })
-  }
+    });
+  };
 
   Action.remoteMethod('upload', {
     description: 'Upload a file',
     accepts: [
       { arg: 'req', type: 'object', http: { source: 'req' } },
-      { arg: 'res', type: 'object', http: { source: 'res' } }
+      { arg: 'res', type: 'object', http: { source: 'res' } },
+      {
+        arg: 'id',
+        type: 'string',
+        required: true
+      },
+      {
+        arg: 'type',
+        type: 'string',
+        required: true
+      }
     ],
     returns: {
       arg: 'result',
       type: 'string'
     },
-    http: { verb: 'post' }
-  })
-}
+    http: { path: '/upload/:type/:id', verb: 'post' }
+  });
+};
--- server/helper-util.js
+++ server/helper-util.js
@@ -1,7 +1,7 @@
-const crypto = require('crypto')
-const algorithm = 'aes-256-cbc'
-const key = crypto.randomBytes(32)
-const iv = crypto.randomBytes(16)
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 // const fs = require('fs');
 // const path = require('path');
 // const _ = require('underscore');
@@ -14,72 +14,77 @@ const url = require('url');
 const randomstring = require('randomstring');
 
 module.exports = {
+  randomId() {
+    const current_date = new Date().valueOf().toString();
+    const random = Math.random().toString();
+    return crypto
+      .createHash('sha1')
+      .update(current_date + random)
+      .digest('hex');
+  },
+  encrypt(text) {
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+  },
 
-  encrypt (text){
-  let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv)
-  let encrypted = cipher.update(text)
-  encrypted = Buffer.concat([encrypted, cipher.final()])
-  return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') }
-},
+  decrypt(text) {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+  },
 
- decrypt (text){
-  let iv = Buffer.from(text.iv, 'hex')
-  let encryptedText = Buffer.from(text.encryptedData, 'hex')
-  let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv)
-  let decrypted = decipher.update(encryptedText)
-  decrypted = Buffer.concat([decrypted, decipher.final()])
-  return decrypted.toString()
-},
+  disableRemoteMethods(Model, moreDisabled = []) {
+    let toBeDisabled = ['deleteById', 'destroyById', 'deleteAll', 'destroyAll'];
+    toBeDisabled = moreDisabled.concat(toBeDisabled);
+    toBeDisabled.forEach(methodName => {
+      Model.disableRemoteMethod(methodName, true);
+    });
+  },
 
-disableRemoteMethods  (Model, moreDisabled = []) {
-  let toBeDisabled = ['deleteById', 'destroyById', 'deleteAll', 'destroyAll']
-  toBeDisabled = moreDisabled.concat(toBeDisabled)
-  toBeDisabled.forEach(methodName => {
-    Model.disableRemoteMethod(methodName, true)
-  })
-},
-
-
-getHostUrlFromReq(req, includePath) {
-  if (includePath) {
+  getHostUrlFromReq(req, includePath) {
+    if (includePath) {
+      return url.format({
+        protocol: req.protocol,
+        hostname: req.hostname,
+        pathname: req.originalUrl
+      });
+    }
     return url.format({
       protocol: req.protocol,
-      hostname: req.hostname,
-      pathname: req.originalUrl,
+      hostname: req.hostname
     });
+  },
+
+  getRandomAlphaNumericString(length, charset) {
+    return randomstring.generate({
+      charset: charset || '0123456789abcdefghijklmnopqrstuvwxyz',
+      length: length || 8
+    });
+  },
+
+  queryDatabase(connector, sql, params = []) {
+    return new Promise((resolve, reject) =>
+      connector.query(sql, params, (err, data) => {
+        if (err) {
+          console.log(err); // eslint-disable-line no-console
+          return reject(err);
+        }
+        return resolve(data);
+      })
+    );
+  },
+
+  getMD5HashSync(data, salt = 'SONAMGUPTABEWAFAHAI') {
+    if (data == null) data = 'N/A'; // eslint-disable-line no-param-reassign
+    data = data.toString() + salt; // eslint-disable-line no-param-reassign
+    return crypto
+      .createHash('md5')
+      .update(data)
+      .digest('hex');
   }
-  return url.format({
-    protocol: req.protocol,
-    hostname: req.hostname,
-  });
-},
-
-getRandomAlphaNumericString(length, charset) {
-  return randomstring.generate({
-    charset: charset || '0123456789abcdefghijklmnopqrstuvwxyz',
-    length: length || 8,
-  });
-},
-
-queryDatabase(connector, sql, params = []) {
-  return new Promise((resolve, reject) =>
-    connector.query(sql, params, (err, data) => {
-      if (err) {
-        console.log(err); // eslint-disable-line no-console
-        return reject(err);
-      }
-      return resolve(data);
-    })
-  );
-},
-
-getMD5HashSync(data, salt = 'SONAMGUPTABEWAFAHAI') {
-  if (data == null) data = 'N/A'; // eslint-disable-line no-param-reassign
-  data = data.toString() + salt; // eslint-disable-line no-param-reassign
-  return crypto
-    .createHash('md5')
-    .update(data)
-    .digest('hex');
-},
-
-}
+};

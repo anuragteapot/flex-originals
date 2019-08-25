20fa561946939e955bd2f3970e5fb5a65e0f4116
anu1601cs <anuragvns1111@gmail.com>
2019-08-26 03:19:47 +0530

    update video

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -5,7 +5,7 @@
     ref="player"
     @mousemove="makeActive(); active = true; "
   >
-    <div class="p-video-loader" v-show="loading">
+    <div :class="`p-video-loader`" v-show="loading">
       <svg
         version="1.1"
         id="L9"
@@ -33,11 +33,84 @@
         </path>
       </svg>
     </div>
-    <video class="p-video-player__media" ref="media">
+    <video class="p-video-player__media" ref="media" @click="playOrPause">
       <source
         src="https://player.vimeo.com/external/194837908.sd.mp4?s=c350076905b78c67f74d7ee39fdb4fef01d12420&profile_id=164"
       />
     </video>
+    <div class="p-video-player-menu">
+      <nav :class="`${active && opensettings ? 'menu-active' : ''}`">
+        <ul v-if="!opensettingspb && !opensettingsq">
+          <li>
+            <a href="#">
+              Auto Play
+              <span class="right">
+                <label class="switch">
+                  <input type="checkbox" />
+                  <div></div>
+                </label>
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href="#" @click="opensettingspb = !  opensettingspb">
+              Playback Speed
+              <span class="right">Normal</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" @click="opensettingsq = !opensettingsq">
+              Quality
+              <span class="right">1080p</span>
+            </a>
+          </li>
+        </ul>
+        <ul v-else-if="opensettingsq">
+          <li>
+            <a href="#" @click="opensettingsq = !  opensettingsq">
+              <i class="fas fa-chevron-left"></i> Quality
+            </a>
+          </li>
+          <li>
+            <a href="#">1080p</a>
+          </li>
+          <li>
+            <a href="#">720p</a>
+          </li>
+          <li>
+            <a href="#">440p</a>
+          </li>
+          <li>
+            <a href="#">114p</a>
+          </li>
+        </ul>
+        <ul v-else>
+          <li>
+            <a href="#" @click="opensettingspb = !  opensettingspb">
+              <i class="fas fa-chevron-left"></i> Playback Speed
+            </a>
+          </li>
+          <li>
+            <a href="#">0.5</a>
+          </li>
+          <li>
+            <a href="#">0.75</a>
+          </li>
+          <li>
+            <a href="#">Normal</a>
+          </li>
+          <li>
+            <a href="#">1.25</a>
+          </li>
+          <li>
+            <a href="#">1.5</a>
+          </li>
+          <li>
+            <a href="#">2</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
     <div :class="`p-video-player__control-panel ${active ? '' : 'show'}`">
       <div></div>
       <div
@@ -68,7 +141,11 @@
           <span class="p-video-player__time-current">{{ getCurrentTime }} / {{getDuration}}</span>
         </div>
         <div class="right">
-          <i class="fas fa-cog"></i>
+          <i class="fas fa-volume-up" @click="volumesettings = !volumesettings"></i>
+          <i
+            :class="`fas fa-cog ${opensettings ? 'rotate-45' : '' }`"
+            @click="opensettings = !opensettings"
+          ></i>
           <i v-if="!fullscreen" class="fas fa-expand" @click="openFullscreen"></i>
           <i v-else class="fas fa-compress" @click="closeFullscreen"></i>
         </div>
@@ -95,6 +172,10 @@ const debounce = function(func, wait, immediate) {
 export default {
   data() {
     return {
+      volumesettings: false,
+      opensettingsq: false,
+      opensettingspb: false,
+      opensettings: false,
       fullscreen: false,
       active: true,
       seekbarWidth: 0,
@@ -156,6 +237,9 @@ export default {
   methods: {
     makeActive: debounce(function() {
       if (this.isPlaying) {
+        this.opensettingsq = false;
+        this.opensettingspb = false;
+        this.opensettings = false;
         this.active = false;
       }
     }, 3000),
@@ -194,6 +278,9 @@ export default {
       this.isPlaying = false;
     },
     playOrPause: function() {
+      this.opensettingsq = false;
+      this.opensettingspb = false;
+      this.opensettings = false;
       if (this.isPlaying) {
         this.active = true;
         this.pause();
@@ -269,11 +356,11 @@ export default {
       }
     },
     seeked() {
-      this.loading = false;
+      // this.loading = false;
       this.time = this.media.currentTime;
     },
     seeking() {
-      this.loading = true;
+      // this.loading = true;
     },
     skipBack() {
       this.media.currentTime += parseFloat(-5);
@@ -282,6 +369,9 @@ export default {
       this.media.currentTime += parseFloat(+5);
     },
     handleEnded() {
+      this.loading = false;
+    },
+    handleProgress() {
       this.loading = false;
     },
     detectKeypress(event) {
@@ -306,9 +396,9 @@ export default {
     // addEventListener
     window.addEventListener(
       "resize",
-      this.$api.debounce(() => {
+      debounce(() => {
         this.reLayoutSeekbar();
-      }),
+      }, 100),
       100
     );
     document.addEventListener("mousemove", event => {
@@ -342,7 +432,10 @@ export default {
 <style lang="scss" scoped>
 .p-video-player {
   width: 100%;
+  position: relative;
+  display: block;
   height: 100%;
+  cursor: pointer;
   color: white !important;
 
   &.no-cursor {
@@ -350,27 +443,25 @@ export default {
   }
 
   .p-video-loader {
-    position: absolute;
     top: 0;
     left: 0;
-    z-index: 5;
     bottom: 0;
     right: 0;
+    z-index: 5;
+    position: absolute;
+    display: inline-block;
     margin: auto;
     height: 100px;
     width: 100px;
   }
 
   .p-video-player__media {
-    position: relative;
     display: block;
     width: 100%;
     height: 100%;
     margin: auto;
     background: #000;
-    font-size: 13px;
     overflow: hidden;
-    font-family: Arial;
   }
 
   .p-video-player__seekbar-wrap {
@@ -399,7 +490,7 @@ export default {
     background-color: rgb(255, 0, 0);
     transform: scaleX(0);
     transform-origin: left;
-    background-color: rgb(112, 110, 110);
+    background-color: rgba(221, 221, 221, 0.8);
   }
   .p-video-player__seekbar-current {
     z-index: 2;
@@ -409,14 +500,15 @@ export default {
   }
 
   .p-video-player__seekbar-back {
-    background-color: #ddd;
+    background-color: rgba(221, 221, 221, 0.4);
   }
 
   .p-video-player__control-panel {
-    top: -40px;
-    padding: 0 20px;
-    position: relative;
-
+    left: 0;
+    right: 0;
+    bottom: 0;
+    padding: 10px 20px;
+    position: absolute;
     background: linear-gradient(
       to bottom,
       rgba(0, 0, 0, 0) 0%,
@@ -424,17 +516,24 @@ export default {
     );
 
     &.show {
-      top: -3px;
-      padding: 0;
-      transition: top 0.3s ease-in-out;
+      padding: 3px 0;
+      background: transparent;
+      transition: bottom 0.5s ease-in-out;
     }
   }
 
   .p-video-player__control-wrap {
     padding: 0 10px;
-    font-size: 15px;
     display: flex;
     justify-content: space-between;
+
+    .rotate-45 {
+      -webkit-transform: rotate(-90deg);
+      -moz-transform: rotate(-90deg);
+      -ms-transform: rotate(-90deg);
+      -o-transform: rotate(-90deg);
+      transform: rotate(-90deg);
+    }
 
     i {
       margin: 0 10px;
@@ -443,6 +542,111 @@ export default {
 
   .p-video-player__time-current {
     margin: 0 0.5em;
+  }
+
+  .p-video-player-menu {
+    position: absolute;
+    bottom: 60px;
+    right: 10px;
+
+    .range-slider {
+      // Slider
+      .input-range {
+        -webkit-appearance: none;
+        height: 200px;
+        border-radius: 5px;
+        background: #ccc;
+        outline: none;
+        writing-mode: bt-lr; /* IE */
+        -webkit-appearance: slider-vertical; /* WebKit */
+      }
+    }
+
+    // Firefox Overrides
+    ::-moz-range-track {
+      background: #ccc;
+      border: 0;
+    }
+
+    input::-moz-focus-inner {
+      border: 0;
+    }
+
+    .switch {
+      display: inline-block;
+      cursor: pointer;
+      font-size: 20px;
+      height: 1em;
+      width: 2em;
+      background: rgb(224, 222, 222);
+      border-radius: 1em;
+
+      input {
+        position: absolute;
+        opacity: 0;
+      }
+
+      div {
+        height: 1em;
+        width: 1em;
+        border-radius: 1em;
+        background: #fff;
+        box-shadow: 0 0.1em 0.3em rgba(0, 0, 0, 0.5);
+        -webkit-transition: all 300ms;
+        -moz-transition: all 300ms;
+        transition: all 300ms;
+      }
+
+      input:checked + div {
+        -webkit-transform: translate3d(100%, 0, 0);
+        -moz-transform: translate3d(100%, 0, 0);
+        transform: translate3d(100%, 0, 0);
+        background-color: red;
+      }
+    }
+
+    nav {
+      opacity: 0;
+      z-index: -1;
+      visibility: hidden;
+      background: rgba(32, 32, 32, 1);
+      transition: 0.2s ease-out;
+      box-shadow: 0 16px 32px rgba(0, 0, 0, 0.25);
+      transform: translateY(50px) scale(0.5);
+      transition: 0.2s;
+      & a {
+        text-decoration: none;
+        color: #fff;
+        display: block;
+        font-size: 12px;
+        padding: 10px;
+        text-align: left;
+
+        .right {
+          float: right;
+        }
+
+        &:hover {
+          background: darken(#414040, 6%);
+          transition: 0.2s ease;
+        }
+      }
+      ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        width: 220px;
+      }
+    }
+
+    .menu-active {
+      opacity: 1;
+      z-index: 6;
+      visibility: visible;
+      transform: translateY(0);
+      transform: translateY(0) scale(1);
+      transition: 0.2s;
+    }
   }
 }
 </style>

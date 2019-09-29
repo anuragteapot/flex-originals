d849fb71705d2d4f11548606d426d73e6fe27b51
anu1601cs <anuragvns1111@gmail.com>
2019-09-30 00:17:02 +0530

    ne

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -3,13 +3,13 @@ DATE=$(date +%Y-%m-%d-%H-%M)
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 mkdir -p backups/$DATE
-sudo docker ps -a | grep 'mongo'
+sudo docker ps -a | grep 'mongo' &> /dev/null
 if [ $? = 0 ]; then
   sudo docker exec -it mongo bash -c "rm -fr /dump ; mkdir /dump ; mongodump -o /dump/"
   sudo docker cp mongo:/dump $SCRIPTPATH/backups/$DATE
   zip -r backups/$DATE.zip ./backups/$DATE
   if [ -f backups/$DATE.zip ]; then
-    rm -fr backups/$DATE
+    sudo rm -fr backups/$DATE
     find $SCRIPTPATH/backups/ -name "*.zip" -mtime +7 -delete
   fi 
 else
--- scripts/db-restore.sh
+++ scripts/db-restore.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 if [ $# -eq 0 ]
   then
-    echo "Supply a path to a zip file!"
+    echo "Supply a path to a tgz file!"
     exit 1
 fi
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 DATE=$(date +%Y-%m-%d-%H-%M)
 
-docker ps -a | grep 'mongo'
+docker ps -a | grep 'mongo' &> /dev/null
 if [ $? = 0 ]; then
 
   if [ -f $1 ]; then
     mkdir -p $SCRIPTPATH/backups/$DATE-restore
-    tar -zx -f $1 -C $SCRIPTPATH/backups/$DATE-restore
-    docker exec -it mongo bash -c "rm -fr /restore ; mkdir /restore"
-    docker cp $SCRIPTPATH/backups/$DATE-restore/mongo mongo:/restore
+    unzip $1 -d $SCRIPTPATH/backups/$DATE-restore
+    # docker exec -it mongo bash -c "rm -fr /restore ; mkdir /restore"
+    # docker cp $SCRIPTPATH/backups/$DATE-restore/flexoriginals mongo:/restore
     ## Only if you get errors about existing indexes, use this instead:
-    ## docker exec -t wekan-db bash -c "mongorestore --drop --noIndexRestore --db wekan /restore/wekan/"
-    docker exec -t wekan-db bash -c "mongorestore --drop --db wekan /restore/wekan/"
+    ## docker exec -t mongo bash -c "mongorestore --drop --noIndexRestore --db flexoriginals /restore/flexoriginals/"
+    # docker exec -t mongo bash -c "mongorestore --drop --db flexoriginals /restore/flexoriginals/"
   fi
 else
-  echo "wekan-db container is not running"
+  echo "mongo container is not running"
   exit 1
 fi

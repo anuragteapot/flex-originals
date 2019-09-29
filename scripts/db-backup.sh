67e2f834d16fa07decadfd2febd749d1de9d4a87
anu1601cs <anuragvns1111@gmail.com>
2019-09-29 22:56:52 +0530

    sufo

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -5,8 +5,8 @@ cd $SCRIPTPATH
 mkdir -p backups/$DATE
 sudo docker ps -a | grep 'mongo'
 if [ $? = 0 ]; then
-  docker exec -it mongo bash -c "rm -fr /dump ; mkdir /dump ; mongodump -o /dump/"
-  docker cp mongo:/dump $SCRIPTPATH/backups/$DATE
+  sudo docker exec -it mongo bash -c "rm -fr /dump ; mkdir /dump ; mongodump -o /dump/"
+  sudo docker cp mongo:/dump $SCRIPTPATH/backups/$DATE
   zip -r backups/$DATE.zip ./backups/$DATE
   if [ -f backups/$DATE.zip ]; then
     rm -fr backups/$DATE

87fa31cb3d688531d18be457abc4b11415cfc460
anu1601cs <anuragvns1111@gmail.com>
2019-10-10 01:26:13 +0530

    new fix

--- server/datasources.staging.js
+++ server/datasources.staging.js
@@ -4,7 +4,7 @@ module.exports = {
   mongodb: {
     host: process.env.MONGODB_HOST || "localhost",
     port: process.env.MONGODB_PORT || 27017,
-    database: process.env.MONGODB_DATABASE || "flexoriginals",
+    database: process.env.MONGODB_DATABASE || "lazydrive",
     user: process.env.MONGODB_DATABASE_USERNAME || "root",
     password: process.env.MONGODB_DATABASE_PASSWORD || "",
     name: "mongodb",

526027707497db36f6cf54059c32095a6b7ffa27
Anurag Kumar <30978328+Anu1601CS@users.noreply.github.com>
2019-10-11 03:04:53 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -10,7 +10,7 @@ const AXIOS_API = () => {
   let instance = null;
   if (process.env.NODE_ENV === 'production') {
     instance = axios.create({
-    baseURL: `http://localhost:3000`,
+    //baseURL: `http://localhost:3000`,
     });
   } else {
     instance = axios.create({

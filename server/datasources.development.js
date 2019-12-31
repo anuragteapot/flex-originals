8cc52118f48e22023d2b88b0a07c3667aaab0290
anu1601cs <anuragvns1111@gmail.com>
2020-01-01 01:38:14 +0530

    fix

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -4,13 +4,13 @@ module.exports = {
   mongodb: {
     host: process.env.MONGODB_HOST || "localhost",
     port: process.env.MONGODB_PORT || 27017,
-    database: process.env.MONGODB_DATABASE || "lazydrive",
+    database: process.env.MONGODB_DATABASE || "flexoriginals",
     user: process.env.MONGODB_DATABASE_USERNAME || "root",
     password: process.env.MONGODB_DATABASE_PASSWORD || "",
     name: "mongodb",
     connector: "mongodb",
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: false,
     connectionTimeout: 3000
   }
 };
--- server/datasources.production.js
+++ server/datasources.production.js
@@ -4,7 +4,7 @@ module.exports = {
   mongodb: {
     host: process.env.MONGODB_HOST || 'mongo',
     port: process.env.MONGODB_PORT || 27017,
-    database: process.env.MONGODB_DATABASE || 'lazydrive',
+    database: process.env.MONGODB_DATABASE || 'flexoriginals',
     user: process.env.MONGODB_DATABASE_USERNAME || 'root',
     password: process.env.MONGODB_DATABASE_PASSWORD || '',
     name: 'mongodb',

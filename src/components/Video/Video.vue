c9b30028658d7b42fc2ae8738f260c9834d8e20f
Anurag Kumar <30978328+Anu1601CS@users.noreply.github.com>
2019-12-31 16:22:51 +0530

    Update Video.vue

--- src/components/Video/Video.vue
+++ src/components/Video/Video.vue
@@ -743,6 +743,10 @@ export default {
     },
     loadedmetadata() {
       this.duration = this.media.duration;
+      if (this.autoPlay) {
+        this.makeActive();
+        this.play();
+      }
     },
     handlePlay() {
       this.isPlaying = true;
@@ -758,10 +762,10 @@ export default {
       this.loading = true;
     },
     handleCanPlay() {
-      if (this.autoPlay) {
-        this.makeActive();
-        this.play();
-      }
+  //    if (this.autoPlay) {
+   //     this.makeActive();
+    //    this.play();
+    //  }
     },
   },
 

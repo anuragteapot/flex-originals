13d70b7b1e336a80470a25b1a43782bc2508da7c
anu1601cs <anuragvns1111@gmail.com>
2019-12-31 17:57:53 +0530

    add script for backups

--- scripts/db-backup.sh
+++ scripts/db-backup.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 DATE=$(date +%Y-%m-%d-%H-%M)
+
+# Register color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;35m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 mkdir -p backups/$DATE
-sudo docker ps -a | grep 'mongo' &> /dev/null
+
+printf "${BLUE}=========================================================================================================${NC}\n"
+echo 'Running docker containers....'
+printf "\n"
+sudo docker ps
+printf "${BLUE}=========================================================================================================${NC}\n"
+echo 'Enter container name : '
+read -p "-> " get_command
+command_type=($(echo $get_command | tr " " "\n"))
+sudo docker ps -a | grep ${command_type[0]} &> /dev/null
 if [ $? = 0 ]; then
-  sudo docker exec -it mongo bash -c "rm -fr /dump ; mkdir /dump ; mongodump -o /dump/"
-  sudo docker cp mongo:/dump $SCRIPTPATH/backups/$DATE
-  zip -r backups/$DATE.zip ./backups/$DATE
-  if [ -f backups/$DATE.zip ]; then
-    sudo rm -fr backups/$DATE
-    find $SCRIPTPATH/backups/ -name "*.zip" -mtime +7 -delete
-  fi 
+  echo ''
+  printf "Starting Backup! -> ${command_type[0]}\n"
+  echo ''
+  docker exec -i ${command_type[0]} sh -c 'mongodump --archive' > $SCRIPTPATH/backups/$DATE/db.dump
+  echo
+  printf "Backup Successfully Done! -> ${command_type[0]} -> ${SCRIPTPATH}/backups/${DATE}/db.dump\n"
+  echo ''
 else
-  echo "mongo container is not running"
+  echo "${command_type[0]} container is not running."
   exit 1
 fi
--- scripts/db-restore.sh
+++ scripts/db-restore.sh
@@ -1,26 +1,57 @@
 #!/bin/bash
-if [ $# -eq 0 ]
-  then
-    echo "Supply a path to a tgz file!"
-    exit 1
-fi
+# if [ $# -eq 0 ]
+#   then
+#     echo "Supply a path to a tgz file!"
+#     exit 1
+# fi
+
+# Register color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;35m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-DATE=$(date +%Y-%m-%d-%H-%M)
+# DATE=$(date +%Y-%m-%d-%H-%M)
 
-docker ps -a | grep 'mongo' &> /dev/null
+echo
+printf "Usage: command list [${BLUE}options${NC}]\n"
+echo
+
+printf "${BLUE}=========================================================================================================${NC}\n"
+echo 'Running docker containers....'
+printf "\n"
+sudo docker ps
+printf "${BLUE}=========================================================================================================${NC}\n"
+echo 'List of avaliable backups : '
+
+printf "  Options : [${BLUE}arguments${NC}]\n"
+echo
+for file in backups/*; do
+    printf "${RED}    -> $(basename "$file")\n${NC}"
+done
+echo
+printf "${BLUE}==========================================${NC}\n"
+echo 
+echo 'Enter container name : '
+read -p "-> " get_command
+command_type=($(echo $get_command | tr " " "\n"))
+printf "${GREEN}-> ${command_type[0]} Container selected!"
+echo 
+echo
+echo 'Select backup dates listed above : '
+read -p "-> " get_command
+backup_date=($(echo $get_command | tr " " "\n"))
+echo
+printf "${GREEN}-> ${backup_date[0]} Dackup date selected!"
+echo 
+
+sudo docker ps -a | grep ${command_type[0]} &> /dev/null
 if [ $? = 0 ]; then
-
-  if [ -f $1 ]; then
-    mkdir -p $SCRIPTPATH/backups/$DATE-restore
-    unzip $1 -d $SCRIPTPATH/backups/$DATE-restore
-    # docker exec -it mongo bash -c "rm -fr /restore ; mkdir /restore"
-    # docker cp $SCRIPTPATH/backups/$DATE-restore/flexoriginals mongo:/restore
-    ## Only if you get errors about existing indexes, use this instead:
-    ## docker exec -t mongo bash -c "mongorestore --drop --noIndexRestore --db flexoriginals /restore/flexoriginals/"
-    # docker exec -t mongo bash -c "mongorestore --drop --db flexoriginals /restore/flexoriginals/"
-  fi
+  sudo docker exec -i ${command_type[0]} sh -c 'mongorestore --archive' < $SCRIPTPATH/backups/$backup_date/db.dump
 else
-  echo "mongo container is not running"
+  echo "${command_type[0]} container is not running."
   exit 1
 fi

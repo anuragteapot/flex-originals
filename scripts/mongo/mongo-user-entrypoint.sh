b4488b07840f871714e8e8da4e29e89c1a8f7929
anu1601cs <anuragvns1111@gmail.com>
2020-01-26 22:00:50 +0530

    env

--- scripts/mongo/mongo-user-entrypoint.sh
+++ scripts/mongo/mongo-user-entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -e;
-echo '-------------------------Creating new user.---------------------'
 
 # a default non-root role
 MONGO_NON_ROOT_ROLE="${MONGO_NON_ROOT_ROLE:-readWrite}"
 
-if [ -n "${APP_MONGO_USER:-}" ] && [ -n "${APP_MONGO_PASS:-}" ]; then
-	"${mongo[@]}" "$APP_MONGO_DB" <<-EOJS
+if [ -n "${MONGO_NON_ROOT_USERNAME:-}" ] && [ -n "${MONGO_NON_ROOT_PASSWORD:-}" ]; then
+	"${mongo[@]}" "$MONGO_INITDB_DATABASE" <<-EOJS
 		db.createUser({
-			user: $(_js_escape "$APP_MONGO_USER"),
-			pwd: $(_js_escape "$APP_MONGO_PASS"),
-			roles: [ { role: $(_js_escape "$MONGO_NON_ROOT_ROLE"), db: $(_js_escape "$APP_MONGO_DB") } ]
+			user: $(_js_escape "$MONGO_NON_ROOT_USERNAME"),
+			pwd: $(_js_escape "$MONGO_NON_ROOT_PASSWORD"),
+			roles: [ { role: $(_js_escape "$MONGO_NON_ROOT_ROLE"), db: $(_js_escape "$MONGO_INITDB_DATABASE") } ]
 			})
 	EOJS
 else
